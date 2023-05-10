c32ac16555415894e59e286584cceee98a330669
AlexanderIvanovColosalTitan <112861088+AlexanderIvanovColosalTitan@users.noreply.github.com>
2023-05-10 22:06:27 +0300

    Add files via upload

---
+++ Н7/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner q = new Scanner(System.in);
+        int num = q.nextInt();
+        System.out.println(Math.round(Math.sqrt(num)));
+
+    }
+}
